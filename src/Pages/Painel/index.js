dc5f85063acd04fc8d93d949397e56fe2143c3b7
alesi93 <silvestrealesi@outlook.com>
2023-11-08 07:46:28 -0300

    Painel Update

--- src/Pages/Painel/index.js
+++ src/Pages/Painel/index.js
@@ -3,11 +3,11 @@ import { Modal, Image, Divider } from 'antd';
 import { Grid } from 'antd';
 import { Typography, Carousel, Space } from 'antd';
 import { Spin } from 'antd';
-
+import { Card } from 'antd';
 
 const { useBreakpoint } = Grid;
 
-function Painel() {
+function Painel () {
   const screens = useBreakpoint();
   const [isModalOpen, setIsModalOpen] = useState(true);
   const [dataLoaded, setDataLoaded] = useState(false);
@@ -29,76 +29,108 @@ function Painel() {
   }, []);
 
   return (
-    <>
-      {dataLoaded ? (
-        <>
-          <Typography.Title level={4}>Seja Bem-Vindo, MEI</Typography.Title>
-          <Divider />
-          <Space direction="vertical">
-            <div>
-              <Typography.Title style={{ color: '#1867DC' }} level={2}>
-                Agora ficou mais fácil!
-              </Typography.Title>
-              <Typography.Text
-                style={{
-                  color: '#444746',
-                  fontWeight: 500,
-                  fontSize: '16px',
-                  paddingBottom: '16px',
-                }}
-              >
-                Fique de olho nos negócios e mantenha tudo sob controle.
+    <div style={{ display: 'flex' }}>
+      {/* Left Container (Existing Content) */}
+      <div style={{ flex: 1 }}>
+        {dataLoaded ? (
+          <>
+            <Typography.Title level={4}>Seja Bem-Vindo, MEI</Typography.Title>
+            <Divider />
+            <Space direction="vertical">
+              <div>
+                <Typography.Title style={{ color: '#1867DC' }} level={2}>
+                  Agora ficou mais fácil!
+                </Typography.Title>
+                <Typography.Text
+                  style={{
+                    color: '#444746',
+                    fontWeight: 500,
+                    fontSize: '16px',
+                    paddingBottom: '16px',
+                  }}
+                >
+                  Fique de olho nos negócios e mantenha tudo sob controle.
+                </Typography.Text>
+              </div>
+              <Typography.Text style={{ color: '#1F1F1F', fontSize: 16, lineHeight: '16px', fontWeight: 480 }}>
+                Aqui você encontra todos os serviços relacionados ao Microempreendedor Individual.
+                <br/>
+                Com o novo programa <b>MEI PI</b> - Portal integrado do Microempreendedor Individual, ficou mais fácil de se organizar agora,
+                concentrando todos os serviços em um único sistema.
               </Typography.Text>
-            </div>
-            <Typography.Text style={{ color: '#1F1F1F', fontSize: 16, lineHeight: '16px', fontWeight: 480 }}>
-              Aqui você encontra todos os serviços relacionados ao Microempreendedor Individual.
-              <br />
-              Com o novo programa <b>MEI PI</b> - Portal integrado do Microempreendedor Individual, ficou mais fácil de se organizar agora, <br></br>concentrando todos os serviços em um único sistema.
-            </Typography.Text>
-            <Image preview={false} src="Hero.png"></Image>
-          </Space>
+              <br></br>
+              <br></br>
+              <Image preview={false} src="Hero.png"></Image>
+            </Space>
+            <Carousel
+              style={{
+                paddingTop: 160,
+                width: 1024,
+                height: '100%',
+              }}
+              autoplay
+              dotActiveWidth
+              dotPosition="bottom"
+            >
+              <div>
+                <Image preview={false} src="https://www.gov.br/casacivil/novopac/novopac/@@govbr.institucional.banner/bbb2c1b0-e99f-4563-aa6b-9ec65137c6a1/@@images/233f5602-163f-4ffc-84dc-4368674119c1.png" />
+              </div>
+              <div>
+                <Image preview={false} src="image1.png" />
+              </div>
+              <div>
+                <Image preview={false} src="https://www.gov.br/casacivil/novopac/novopac/@@govbr.institucional.banner/bbb2c1b0-e99f-4563-aa6b-9ec65137c6a1/@@images/233f5602-163f-4ffc-84dc-4368674119c1.png" />
+              </div>
+            </Carousel>
+            {breakpointsToRender.map((breakpoint) =>
+              screens[breakpoint] && (
+                <Modal
+                  className="Modal1"
+                  title="Seja Bem-Vindo MEI"
+                  visible={isModalOpen}
+                  onOk={handleOk}
+                  onCancel={handleCancel}
+                  key={breakpoint}
+                  cancelText="Cancelar"
+                >
+                  <Image className="cardPic" style={{ padding: '8px 0 0' }} preview={false} src="MainCard2.png" />
+                </Modal>
+              )
+            )}
+          </>
+        ) : (
+          <div style={{ background: "white" }} className='SpinnerContainer'><Spin /></div>
+        )}
+      </div>
 
-          <Carousel
-            style={{
-              paddingTop: 160,
-              width: 1024,
-              height: '100%',
-            }}
-            autoplay
-            dotActiveWidth
-            dotPosition="bottom"
-          >
-            <div>
-              <Image preview={false} src="https://www.gov.br/casacivil/novopac/novopac/@@govbr.institucional.banner/bbb2c1b0-e99f-4563-aa6b-9ec65137c6a1/@@images/233f5602-163f-4ffc-84dc-4368674119c1.png" />
-            </div>
-            <div>
-              <Image preview={false} src="image1.png" />
-            </div>
-            <div>
-              <Image preview={false} src="https://www.gov.br/casacivil/novopac/novopac/@@govbr.institucional.banner/bbb2c1b0-e99f-4563-aa6b-9ec65137c6a1/@@images/233f5602-163f-4ffc-84dc-4368674119c1.png" />
-            </div>
-          </Carousel>
+      {/* A página Home inicialmente não existiria, porém, para uma melhor imersão no desafio, foi criada da forma que foi possível com o tempo disposto */}
+      <div style={{ flex: 1, background: 'white' }}>
+        {/* Sessão para as últimas notícias, infelizmente os padrões de interface não conseguiram ser atingidos com o tempo disponível */}
+        <Typography.Title style={{
+          color:'white'
+          }} level={4}>null</Typography.Title>
+          
+        <Divider></Divider>
+        <br></br>
+          
 
-          {breakpointsToRender.map((breakpoint) =>
-            screens[breakpoint] && (
-              <Modal
-                className="Modal1"
-                title="Seja Bem-Vindo MEI"
-                visible={isModalOpen}
-                onOk={handleOk}
-                onCancel={handleCancel}
-                key={breakpoint}
-                cancelText="Cancelar"
-              >
-                <Image className="cardPic" style={{ padding: '8px 0 0' }} preview={false} src="MainCard2.png" />
-              </Modal>
-            )
-          )}
-        </>
-      ) : (
-        <div style={{background:"white",}}className='SpinnerContainer'><Spin /></div>
-      )}
-    </>
+        <Typography.Title style={{marginLeft:60,}}level={4}>Últimas Notícias</Typography.Title>
+        <div>
+        <Card className="CardsNews" style={{ display: 'flex', alignItems: 'center' }}>
+        <Image preview={false} src="https://agenciagov.ebc.com.br/noticias/202311/direitos-da-populacao-quilombola-e-pauta-da-voz-do-brasil-desta-terca-feira-07/@@images/image/preview"/>
+        </Card>
+        <Card className="CardsNews" style={{ display: 'flex', alignItems: 'center' }}>
+        <Image preview={false} src="https://agenciagov.ebc.com.br/noticias/202311/saiba-o-que-acontece-com-quem-faltou-no-primeiro-dia-de-enem/@@images/image/preview"/>
+        </Card>
+        <Card  className="CardsNews" style={{ display: 'flex', alignItems: 'center' }}>
+        <Image preview={false} src="https://agenciagov.ebc.com.br/noticias/202311/ministerio-da-saude-simplifica-regras-para-envio-de-documentos-na-inscricao-do-novo-pac-saude/@@images/image/preview"/>
+        </Card>
+        </div>
+        
+        
+        
+      </div>
+    </div>
   );
 }
 

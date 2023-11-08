837975567a338d17d10e85eaf3cf8cebb21e736c
alesi93 <silvestrealesi@outlook.com>
2023-11-08 20:46:55 -0300

    Update index.js

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Image, Divider } from 'antd';
 import { Grid } from 'antd';
-import { Typography, Carousel, Space } from 'antd';
+import { Typography, Space } from 'antd';
 import { Spin } from 'antd';
-import { Card } from 'antd';
+
 
 const { useBreakpoint } = Grid;
 
@@ -29,9 +29,9 @@ function Home() {
   }, []);
 
   return (
-    <div style={{ display: 'flex' }}>
+    <div className='' style={{ display: 'flex' }}>
      
-      <div style={{ flex: 1 }}>
+      <div className='content-container' style={{ flex: 1 }}>
         {dataLoaded ? (
           <>
             <Typography.Title level={4}>Seja Bem-Vindo, MEI</Typography.Title>
@@ -53,35 +53,16 @@ function Home() {
               </div>
               <Typography.Text style={{ color: '#1F1F1F', fontSize: 18, lineHeight: '16px', fontWeight: 480 }}>
                 Aqui você encontra todos os serviços relacionados ao Microempreendedor Individual.
-                <br/>
+               
                 Com o novo programa <b>MEI PI</b> - Portal integrado do Microempreendedor Individual, 
-                ficou mais fácil de se organizar<br></br> agora,
+                ficou mais fácil de se organizar agora,
                 concentrando todos os serviços em um único sistema.
               </Typography.Text>
               <br></br>
               <br></br>
-              <Image preview={false} src="Hero.png"></Image>
+              
             </Space>
-            <Carousel
-              style={{
-                paddingTop: 160,
-                width: 1024,
-                height: '100%',
-              }}
-              autoplay
-              dotActiveWidth
-              dotPosition="bottom"
-            >
-              <div>
-                <Image preview={false} src="https://www.gov.br/casacivil/novopac/novopac/@@govbr.institucional.banner/bbb2c1b0-e99f-4563-aa6b-9ec65137c6a1/@@images/233f5602-163f-4ffc-84dc-4368674119c1.png" />
-              </div>
-              <div>
-                <Image preview={false} src="image1.png" />
-              </div>
-              <div>
-                <Image preview={false} src="https://www.gov.br/casacivil/novopac/novopac/@@govbr.institucional.banner/bbb2c1b0-e99f-4563-aa6b-9ec65137c6a1/@@images/233f5602-163f-4ffc-84dc-4368674119c1.png" />
-              </div>
-            </Carousel>
+            
             {breakpointsToRender.map((breakpoint) =>
               screens[breakpoint] && (
                 <Modal
@@ -104,32 +85,7 @@ function Home() {
       </div>
 
       {/* A página Home inicialmente não existiria, porém, para uma melhor imersão no desafio, foi criada da forma que foi possível com o tempo disposto */}
-      <div style={{ flex: 1, background: 'white' }}>
-        {/* Sessão para as últimas notícias, infelizmente os padrões de interface não conseguiram ser atingidos com o tempo disponível */}
-        <Typography.Title style={{
-          color:'white'
-          }} level={4}>null</Typography.Title>
-          
-        <Divider></Divider>
-        <br></br>
-          
 
-        <Typography.Title style={{marginLeft:60,}}level={4}>Últimas Notícias</Typography.Title>
-        <div>
-        <Card className="CardsNews" style={{ display: 'flex', alignItems: 'center' }}>
-        <Image preview={false} src="https://agenciagov.ebc.com.br/noticias/202311/direitos-da-populacao-quilombola-e-pauta-da-voz-do-brasil-desta-terca-feira-07/@@images/image/preview"/>
-        </Card>
-        <Card className="CardsNews" style={{ display: 'flex', alignItems: 'center' }}>
-        <Image preview={false} src="https://agenciagov.ebc.com.br/noticias/202311/saiba-o-que-acontece-com-quem-faltou-no-primeiro-dia-de-enem/@@images/image/preview"/>
-        </Card>
-        <Card  className="CardsNews" style={{ display: 'flex', alignItems: 'center' }}>
-        <Image preview={false} src="https://agenciagov.ebc.com.br/noticias/202311/ministerio-da-saude-simplifica-regras-para-envio-de-documentos-na-inscricao-do-novo-pac-saude/@@images/image/preview"/>
-        </Card>
-        </div>
-        
-        
-        
-      </div>
     </div>
   );
 }

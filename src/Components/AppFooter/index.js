6827f3f291afab4e8fa8f047685fdf5e3f067ca8
alesi93 <silvestrealesi@outlook.com>
2023-11-09 13:09:48 -0300

    Update index.js

--- src/Components/AppFooter/index.js
+++ src/Components/AppFooter/index.js
@@ -2,9 +2,9 @@ import { Typography } from "antd";
 
 function AppFooter () {
     return ( <div className="AppFooter">
-        <Typography.Link href="https://www.gov.br" target={'_blank'}>Gov.br</Typography.Link>
-        <Typography.Link href="https://www.gov.br" target={'_blank'}>Privacidade</Typography.Link>
-        <Typography.Link href="https://www.gov.br" target={'_blank'}>Termos de Uso</Typography.Link>
+        <Typography.Link style={{color:'#183EFF',}} href="https://www.gov.br" target={'_blank'}>Gov.br</Typography.Link>
+        <Typography.Link style={{color:'#183EFF',}} href="https://www.gov.br" target={'_blank'}>Privacidade</Typography.Link>
+        <Typography.Link style={{color:'#183EFF',}} href="https://www.gov.br" target={'_blank'}>Termos de Uso</Typography.Link>
         
     
     </div>
